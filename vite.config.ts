2b89316e231f9018c4b0e14a5f0dd1f05f01dc22
sayandkk <108538779+sayandkk@users.noreply.github.com>
2025-07-30 12:25:04 +0530

    bug

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Portfolio/', // 👈 IMPORTANT for GitHub Pages
+  base: '/', // 👈 IMPORTANT for GitHub Pages
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
